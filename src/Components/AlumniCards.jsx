30909ae24bf1a1a0c46b5fd35e90b844e4c6bbd4
Siddharthkumarrrrr <Siddharthkmarrrrr@gmail.com>
2024-07-05 12:30:39 +0530

    updated Dashboard

--- src/Components/AlumniCards.jsx
+++ src/Components/AlumniCards.jsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAlumni } from '../features/alumni/alumniSlice';
 
+const getTransformedImageUrl = (url) => {
+  const cloudinaryBaseUrl = "https://res.cloudinary.com/dsq1uhh6a/image/upload";
+  const transformations = "w_300,h_250,c_fill,q_auto,f_auto";
+  
+  // Assuming the URL format is https://res.cloudinary.com/your_cloud_name/image/upload/v1234567890/sample.jpg
+  const parts = url.split("/upload/");
+  return `${cloudinaryBaseUrl}/${transformations}/${parts[1]}`;
+};
 const AlumniCards = () => {
   const dispatch = useDispatch();
   const alumni = useSelector((state) => state.alumni.alumni);
@@ -26,7 +34,7 @@ const AlumniCards = () => {
         (searchName === '' || person.name.toLowerCase().includes(searchName.toLowerCase())) &&
         (searchGraduationYear === '' || person.graduationYear === parseInt(searchGraduationYear)) &&
         (searchBranch === '' || person.branch.toLowerCase().includes(searchBranch.toLowerCase())) &&
-        (searchCompany === '' || person.company.toLowerCase().includes(searchCompany.toLowerCase()))
+        (searchCompany === '' || (person.company && person.company.toLowerCase().includes(searchCompany.toLowerCase())))
       );
     });
     setFilteredAlumni(filtered);
@@ -64,7 +72,7 @@ const AlumniCards = () => {
           className="border p-2 m-2"
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 border-black lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-3 border-black lg:grid-cols-5 gap-6 px-10">
         {alumniStatus === 'loading' && <p>Loading...</p>}
         {alumniStatus === 'succeeded' &&
           filteredAlumni.map((person, index) => (
@@ -73,10 +81,11 @@ const AlumniCards = () => {
               className="bg-white shadow-md border-2 border-black rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300"
             >
               <img
-                src={person.image}
+                src={getTransformedImageUrl(person.image)}
                 alt={person.name}
                 className="w-full h-auto object-cover rounded-t-lg"
-                style={{ maxHeight: '500px' }} // Set a max height to limit excessive stretching
+                style={{ maxHeight: '500px' }} 
+                loading='lazy'
               />
               <div className="p-4">
                 <h5 className="text-xl font-bold">{person.name}</h5>
