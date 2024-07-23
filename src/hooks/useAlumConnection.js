d2ceae99c2060166efbfa14125a70567853763a4
Siddharthkumarrrrr <Siddharthkmarrrrr@gmail.com>
2024-07-23 12:26:07 +0530

    new_user

--- src/hooks/useAlumConnection.js
+++ src/hooks/useAlumConnection.js
@@ -7,38 +7,42 @@ import GetConversations from "./useGetConversations";
 import useConversation from "../zustand/useConversation";
 
 const useAlumConnection = (person) => {
-    const {  authUser } = useAuthContext();
+    const { authUser } = useAuthContext();
     const navigate = useNavigate();
     const { setSelectedConversation } = useConversation();
     const dispatch = useDispatch();
     const { loading, conversations } = useSelector(state => state.conversations);
 
     useEffect(() => {
-        if (!person) return;
+        const fetchConversations = async () => {
+            if (conversations.length === 0 && !loading) {
+                await dispatch(GetConversations());
+
+                // No need for an extra state check, rely on `conversations` to update after dispatch
+                if (conversations.length === 0) {
+                    dispatch(addConversation(person));
+                }
+            } else {
+                const existingConversation = conversations.find(conv => conv._id === person._id);
+                if (!existingConversation) {
+                    dispatch(addConversation(person));
+                }
+            }
+
+            setSelectedConversation(person);
+            navigate('/messages');
+        };
+
+        if (!person) return; // 1. If no person, return
 
         if (!authUser) {
-            navigate('/AlumniLogin');
+            navigate('/AlumniLogin'); // 2. If not authUser, navigate to login
             return;
         }
 
-        if (!loading && conversations.length === 0) {
-            dispatch(GetConversations());
-        }
-    }, [authUser, dispatch, loading, conversations, navigate, person]);
+        fetchConversations();
 
-    useEffect(() => {
-        if (!person) return;
-
-        if (authUser && conversations.length > 0) {
-            const existingConversation = conversations.find(conv => conv._id === person._id);
-            if (!existingConversation) {
-                dispatch(addConversation(person));
-            }
-            setSelectedConversation(person);
-
-            navigate('/messages');
-        }
-    }, [authUser, conversations, person, setSelectedConversation, dispatch, navigate]);
+    }, [authUser, dispatch, loading, conversations, navigate, person, setSelectedConversation]);
 };
 
 export default useAlumConnection;
